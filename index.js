688bba2a137b8905271ad402a4bf7d86951cff2e
timurizbasarov <timurizbasarov0703@gmail.com>
2024-05-29 21:50:37 +0600

    love

---
+++ index.js
@@ -0,0 +1,18 @@
+const textElement = document.getElementById("text");
+const speedElement = document.getElementById("speed");
+const text = "i love ?"
+let index = 1;
+let speed = 300 / speedElement.ariaValueMax;
+
+const writeText = () => {
+    textElement.innerText = text.slice(0, index);
+    index++;
+    if (index > text.length) index = 1;
+    setTimeout(writeText, speed);
+};
+
+writeText();
+
+speedElement.addEventListener("input", (e) => (
+speed = 300 / e.target.value)
+);
